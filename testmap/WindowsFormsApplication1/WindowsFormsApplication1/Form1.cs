0cb0f3a0fe3ac36476608d9e59609613bcc47284
bartca <bartcamies@gmail.com>
2016-11-28 10:24:09 +0100

    testmap
    testmap met testapplicatie, zie diemoetjedoen.txt

---
+++ testmap/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("C:/Users/carroto/Documents/Visual Studio 2013/Projects/WindowsFormsApplication1/WindowsFormsApplication1/testpage.html");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int flySec = Convert.ToInt32(textBox1.Text);
+
+            object[] o = new object[1];
+            o[0] = flySec;
+
+            webBrowser1.Document.InvokeScript("fly", o);
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser wb = new WebBrowser();
+            wb.AllowNavigation = true;
+
+            wb.Navigate("testpage.html");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
